cdc26350cb303e57cc9629da8ff6a5f0f32b191c
Dan Schuler <104746354+dandanthesoftwareman@users.noreply.github.com>
2022-08-01 23:07:15 -0400

    second commit

--- country.js
+++ country.js
@@ -6,14 +6,25 @@ class Country {
         this.colors = colors;
     }
 }
-let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"]);
+let usa = new Country("USA", "English", "Hello World!", ["red", "white", "blue"]);
 let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
-let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
+let algeria = new Country("Algeria", "Arabic and Berber", "marhaban bialealam!", ["blue", "yellow", "red"])
+
+// let countryNameNode = document.getElementById("CountryName");
+// let OfficialLanguageNode = document.getElementById("OfficialLanguage");
+// let HelloWorld = document.getElementById("HelloWorld");
 
 
 function SwitchCountry() {
-    let input = "USA";
-    let country;
+    let input = document.getElementById("CountryList").value;
+    let countryNameNode = document.getElementById("CountryName");
+    let OfficialLanguageNode = document.getElementById("OfficialLanguage");
+    let HelloWorld = document.getElementById("HelloWorld");
+    let colorNode1 = document.getElementById("Color1");
+    let colorNode2 = document.getElementById("Color2");
+    let colorNode3 = document.getElementById("Color3");
+    let country = usa;
+    // console.log(input);
 
     if (input === "USA") {
         //set country to usa 
@@ -25,5 +36,11 @@ function SwitchCountry() {
     else if (input === "Algeria") {
         country = algeria;
     }
+    countryNameNode.innerText = country.name;
+    OfficialLanguageNode.innerText = country.lang;
+    HelloWorld.innerText = country.greeting;
+    colorNode1.style.backgroundColor = country.colors[0];
+    colorNode2.style.backgroundColor = country.colors[1];
+    colorNode3.style.backgroundColor = country.colors[2];
 
 }
